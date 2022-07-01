f4d7afbc2e516c186bf8bbf75286332a5982a871
Archana <archanar249@gmail.com>
2022-07-01 15:51:50 +0530

    Added to-do-list program

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+import { useState } from 'react';
+import TodoList from "./Components/To-do-list";
+
+function App() {
+
+  const[currentItem,setCurrentItem] = useState(null);
+  const[itemList,updateItemList] = useState([]);
+
+  const onChangeHandler = e => {
+    setCurrentItem(e.target.value);
+  };
+  const addItemToList = () => {
+
+    updateItemList([...itemList, {item:currentItem,  key: Date.now()}]);
+    setCurrentItem("");
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='Wrapper'>
+          <div className='Input-wrapper'>
+            <input 
+            placeholder="Add item"
+            value={currentItem} onChange={onChangeHandler}/> 
+            <button onClick={addItemToList}>+</button> 
+          </div>
+          <TodoList itemList={itemList} updateItemList={updateItemList} />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
